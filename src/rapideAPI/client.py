3d08d1aeb1d5dcbea287ff0d83ce7c94122e5b03
lejih <julien.berson@epitech.eu>
2026-09-24 21:58:09 +0200

    refacto(client): added client stream option

--- src/rapideAPI/client.py
+++ src/rapideAPI/client.py
@@ -60,7 +60,7 @@ class RapideAPI:
     def get(self, endpoint: str, params: Optional[Dict[str, Any]] = None, **kwargs) -> Any:
         return self.request("GET", endpoint, params=params, **kwargs)
 
-    def post(self, endpoint: str, data: Optional[Union[Dict, str]] = None, json: Optional[Dict] = None, **kwargs) -> Any:
+    def post(self, endpoint: str, data: Optional[Union[Dict, str]] = None, json: Optional[Dict] = None, stream: bool = False, **kwargs) -> Any:
         return self.request("POST", endpoint, data=data, json=json, **kwargs)
 
     def put(self, endpoint: str, data: Optional[Union[Dict, str]] = None, json: Optional[Dict] = None, **kwargs) -> Any:
